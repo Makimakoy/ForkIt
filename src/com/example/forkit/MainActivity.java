925e286ca4507f08d310a8100da6209662d52228
maki <mark.grancapal@gmail.com>
2013-08-10 21:10:40 +0800

    Edit

---
+++ src/com/example/forkit/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.forkit;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void buttonClick(View vv){
+		if(vv.getId()==R.id.btn_map){
+			startActivity(new Intent());
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
